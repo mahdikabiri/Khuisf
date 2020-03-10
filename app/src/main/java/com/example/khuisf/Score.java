964b5a29437b89edfa7a5de9d968035a60c9edf8
mahdi kabiri <mahdi@gmail.com>
2020-03-10 17:51:08 +0330

    get score for student

---
+++ app/src/main/java/com/example/khuisf/Score.java
@@ -0,0 +1,28 @@
+package com.example.khuisf;
+
+public class Score {
+    String courseName,unitTeori,unitAmali,score;
+
+    public Score(String courseName, String unitTeori, String unitAmali, String score) {
+        this.courseName = courseName;
+        this.unitTeori = unitTeori;
+        this.unitAmali = unitAmali;
+        this.score = score;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public String getUnitTeori() {
+        return unitTeori;
+    }
+
+    public String getUnitAmali() {
+        return unitAmali;
+    }
+
+    public String getScore() {
+        return score;
+    }
+}
---
+++ app/src/main/java/com/example/khuisf/ScoreAdapter.java
@@ -0,0 +1,77 @@
+package com.example.khuisf;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class ScoreAdapter extends RecyclerView.Adapter<ScoreAdapter.ScoreViewHolder> {
+
+    LayoutInflater inflater;
+    Context context;
+    private ArrayList<Score> mCourses;
+
+    public ScoreAdapter(Context context, ArrayList<Score> courses) {
+        this.context = context;
+        mCourses = courses;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public ScoreViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.getcourse_item, parent, false);
+        return new ScoreViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ScoreViewHolder holder, int position) {
+        Score score = mCourses.get(position);
+        holder.tvTitle.setText(score.getCourseName());
+        holder.tvUnitTeori.setText(score.getUnitTeori());
+        holder.tvUnitAmali.setText(score.getUnitAmali());
+        holder.tvScore.setText(score.getScore());
+
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              /*  Intent intent=new Intent(context, CourseInfoActivity.class);
+                intent.putExtra(MainActivity.CHARAC,currentItem.getCharac());
+                intent.putExtra(MainActivity.NAME,currentItem.getTitle());
+                intent.putExtra(MainActivity.DAY,currentItem.getDay());
+                intent.putExtra(MainActivity.TIME,currentItem.getTime());
+
+                context.startActivity(intent);
+                Toast.makeText(context, holder.tvTime.getText(), Toast.LENGTH_SHORT).show();*/
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public static class ScoreViewHolder extends RecyclerView.ViewHolder {
+        public TextView tvTitle;
+        public TextView tvUnitAmali;
+        public TextView tvUnitTeori;
+        public TextView tvScore;
+
+        public ScoreViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvTitle = itemView.findViewById(R.id.item_getscore_tv_title);
+            tvUnitTeori = itemView.findViewById(R.id.item_getscore_unit_teori);
+            tvUnitAmali = itemView.findViewById(R.id.item_getscore_unit_amali);
+            tvScore = itemView.findViewById(R.id.item_getscore_tv_score);
+        }
+    }
+
+}
